7da0f0dc032538f7ca107f034804c278567536d7
newhackerman <newhackerman@163.com>
2020-12-06 16:47:11 +0800

    对于读到__字符做0处理

--- 早盘数据入库.py
+++ 早盘数据入库.py
@@ -43,47 +43,119 @@ date
     row_number = sheet_1.nrows #获取有数据的最大行数
     j=0 #控制提交频率
     for i in range(1,row_number):
-        j+=1
-        code = sheet_1.cell(i,0).value
-        name = sheet_1.cell(i,1).value
-        zhangfu = sheet_1.cell(i,2).value
-        kaipanhuanshuoz= sheet_1.cell(i,3).value
-        kaipanjine =  sheet_1.cell(i,4).value
-        huanshuonu = sheet_1.cell(i,6).value
-        liangbi = sheet_1.cell(i,7).value
-        xianliang = sheet_1.cell(i,9).value
-        zongliang = sheet_1.cell(i,10).value
-        zongjine =sheet_1.cell(i,11).value
-        xianjia =sheet_1.cell(i,12).value
-        junjia = sheet_1.cell(i,13).value
-        liutongguyi = sheet_1.cell(i,14).value
-        liutongsizhi =sheet_1.cell(i,15).value
-        renjiusizhi = sheet_1.cell(i,16).value
-        xifenhangye = sheet_1.cell(i,21).value
-        diqu = sheet_1.cell(i,22).value
-        siyingniu = sheet_1.cell(i,24).value
-        huoyuedu = sheet_1.cell(i,34).value
-        lianzhangtiansu=sheet_1.cell(i,36).value
-        shanrizhangfu = sheet_1.cell(i,37).value
-        ershirizhangfu = sheet_1.cell(i,38).value
-        liushirizhangfu=sheet_1.cell(i,39).value
-        huanshuoz = sheet_1.cell(i,41).value
-        liutongsizhiz = sheet_1.cell(i,42).value
-        #print(liutongsizhi)
-        beitaxishuo =sheet_1.cell(i,45).value
-        kaipan =sheet_1.cell(i,49).value
-        gudongrenshuo = sheet_1.cell(i,84).value
-        renjunchigu = sheet_1.cell(i,85).value
-        liruntongbi = sheet_1.cell(i,86).value
-        shuyutongbi =sheet_1.cell(i,87).value
-        shijingniu = sheet_1.cell(i,88).value
-        meigujingzhi =sheet_1.cell(i,93).value
-        meigugongji = sheet_1.cell(i,95).value
-        meiguweifenpei =sheet_1.cell(i,96).value
-        meiguxianjinliu=sheet_1.cell(i,97).value
-        maoliniu = sheet_1.cell(i,100).value
-        yinyeilirunniu =sheet_1.cell(i,101).value
-        jinlirunniu = sheet_1.cell(i,102).value
+        j += 1
+        code = sheet_1.cell(i, 0).value
+        name = sheet_1.cell(i, 1).value
+        zhangfu = sheet_1.cell(i, 2).value
+        if '--' in str(zhangfu):
+            zhangfu = 0
+        kaipanhuanshuoz = sheet_1.cell(i, 3).value
+        if '--' in str(kaipanhuanshuoz):
+            kaipanhuanshuoz = 0
+        kaipanjine = sheet_1.cell(i, 4).value
+        if '--' in str(kaipanjine):
+            kaipanjine = 0
+        huanshuonu = sheet_1.cell(i, 6).value
+        if '--' in str(huanshuonu):
+            huanshuonu = 0
+        liangbi = sheet_1.cell(i, 7).value
+        if '--' in str(liangbi):
+            liangbi = 0
+        xianliang = sheet_1.cell(i, 9).value
+        if '--' in str(xianliang):
+            xianliang = 0
+        zongliang = sheet_1.cell(i, 10).value
+        if '--' in str(zongliang):
+            zongliang = 0
+        zongjine = sheet_1.cell(i, 11).value
+        if '--' in str(zongjine):
+            zongjine = 0
+        xianjia = sheet_1.cell(i, 12).value
+        if '--' in str(xianjia):
+            xianjia = 0
+        junjia = sheet_1.cell(i, 13).value
+        if '--' in str(junjia):
+            junjia = 0
+        liutongguyi = sheet_1.cell(i, 14).value
+        if '--' in str(liutongguyi):
+            liutongguyi = 0
+        liutongsizhi = sheet_1.cell(i, 15).value
+        if '--' in str(liutongsizhi):
+            liutongsizhi = 0
+        renjiusizhi = sheet_1.cell(i, 16).value
+        if '--' in str(renjiusizhi):
+            renjiusizhi = 0
+        xifenhangye = sheet_1.cell(i, 21).value
+        diqu = sheet_1.cell(i, 22).value
+        siyingniu = sheet_1.cell(i, 24).value
+        if '--' in str(siyingniu):
+            siyingniu = 0
+        huoyuedu = sheet_1.cell(i, 34).value
+        if '--' in str(huoyuedu):
+            huoyuedu = 0
+        lianzhangtiansu = sheet_1.cell(i, 36).value
+        if '--' in str(lianzhangtiansu):
+            lianzhangtiansu = 0
+        shanrizhangfu = sheet_1.cell(i, 37).value
+        if '--' in str(shanrizhangfu):
+            shanrizhangfu = 0
+        ershirizhangfu = sheet_1.cell(i, 38).value
+        if '--' in str(ershirizhangfu):
+            ershirizhangfu = 0
+        liushirizhangfu = sheet_1.cell(i, 39).value
+        if '--' in str(liushirizhangfu):
+            liushirizhangfu = 0
+        huanshuoz = sheet_1.cell(i, 41).value
+        if '--' in str(huanshuoz):
+            huanshuoz = 0
+        liutongsizhiz = sheet_1.cell(i, 42).value
+        if '--' in str(liutongsizhiz):
+            liutongsizhiz = 0
+        # print(liutongsizhi)
+        beitaxishuo = sheet_1.cell(i, 45).value
+        if '--' in str(beitaxishuo):
+            beitaxishuo = 0
+        kaipan = sheet_1.cell(i, 49).value
+        if '--' in str(kaipan):
+            kaipan = 0
+        gudongrenshuo = sheet_1.cell(i, 84).value
+        if '--' in str(gudongrenshuo):
+            gudongrenshuo = 0
+        renjunchigu = sheet_1.cell(i, 85).value
+        if '--' in str(renjunchigu):
+            renjunchigu = 0
+        liruntongbi = sheet_1.cell(i, 86).value
+        if '--' in str(liruntongbi):
+            liruntongbi = 0
+        shuyutongbi = sheet_1.cell(i, 87).value
+        if '--' in str(shuyutongbi):
+            shuyutongbi = 0
+        shijingniu = sheet_1.cell(i, 88).value
+        if '--' in str(shijingniu):
+            shijingniu = 0
+        meigujingzhi = sheet_1.cell(i, 93).value
+        if '--' in str(meigujingzhi):
+            meigujingzhi = 0
+        meigugongji = sheet_1.cell(i, 95).value
+        if '--' in str(meigugongji):
+            meigugongji = 0
+        meiguweifenpei = sheet_1.cell(i, 96).value
+        if '--' in str(meiguweifenpei):
+            meiguweifenpei = 0
+        meiguxianjinliu = sheet_1.cell(i, 97).value
+        if '--' in str(meiguxianjinliu):
+            meiguxianjinliu = 0
+        maoliniu = sheet_1.cell(i, 100).value
+        #print(maoliniu)
+        if '--' in str(maoliniu):
+            maoliniu = 0
+            #print(maoliniu)
+        yinyeilirunniu = sheet_1.cell(i, 101).value
+        if '--' in str(yinyeilirunniu):
+            yinyeilirunniu = 0
+        jinlirunniu = sheet_1.cell(i, 102).value
+        if '--' in str(jinlirunniu):
+            jinlirunniu = 0
         date=str(date1)
         values = (code,name,zhangfu,kaipanhuanshuoz,kaipanjine,huanshuonu,liangbi,xianliang,zongliang, zongjine,
                   xianjia,junjia,liutongguyi, liutongsizhi ,renjiusizhi ,xifenhangye,diqu , siyingniu,huoyuedu,
@@ -125,48 +197,48 @@ if __name__ == '__main__':
 CREATE TABLE IF NOT EXISTS `stockopendata`(           
 `code`            varchar(8),                   //代码,       
 `name`            varchar(12),                  //名称,       
-`zhangfu`         varchar(10),                 // 涨幅%,      
-`kaipanhuanshuoz` varchar(10),                 // 开盘换手Z,  
-`kaipanjine`      varchar(10),                 // 开盘金额,   
-`huanshuonu`      varchar(10),                 // 换手%,      
-`liangbi`         varchar(10),                 // 量比,       
-`xianliang`       varchar(10),                 // 现量,       
-`zongliang`       varchar(10),                  //总量,       
-`zongjine`        varchar(10),                 // 总金额,     
-`xianjia`         varchar(10),                 // 现价,       
-`junjia`          varchar(10),                 // 均价,       
-`liutongguyi`     varchar(10),                 // 流通股(亿), 
-`liutongsizhi`    varchar(10),                 // 流通市值,   
+`zhangfu`         float,                 // 涨幅%,      
+`kaipanhuanshuoz` float,                 // 开盘换手Z,  
+`kaipanjine`      float,                 // 开盘金额,   
+`huanshuonu`      float,                 // 换手%,      
+`liangbi`         float,                 // 量比,       
+`xianliang`       float,                 // 现量,       
+`zongliang`       float,                  //总量,       
+`zongjine`        float,                 // 总金额,     
+`xianjia`         float,                 // 现价,       
+`junjia`          float,                 // 均价,       
+`liutongguyi`     varchar(15),                 // 流通股(亿), 
+`liutongsizhi`    varchar(15),                 // 流通市值,   
 `renjiusizhi`     varchar(10),                 // 人均市值,   
 `xifenhangye`     varchar(10),                 // 细分行业,   
 `diqu`            varchar(10),                  //地区,       
-`siyingniu`       varchar(10),                 // 市盈(动,    
-`huoyuedu`        varchar(10),                  //活跃度,     
-`lianzhangtiansu` varchar(10),                 // 连涨天,     
-`shanrizhangfu`   varchar(10),                  //3日涨幅%,   
-`ershirizhangfu`  varchar(10),                 // 20日涨幅%,  
-`liushirizhangfu` varchar(10),                 // 60日涨幅%,  
-`huanshuoz`       varchar(10),               //   换手Z,      
-`liutongsizhiz`   varchar(10),                 // 流通市值Z,  
-`beitaxishuo`     varchar(10),                  //贝塔系数,   
-`kaipan`          varchar(10),                 // 开盘%,      
-`gudongrenshuo`   varchar(10),                 // 股东人数,   
-`renjunchigu`     varchar(10),                 // 人均持股,   
-`liruntongbi`     varchar(10),                //  利润同比%,  
-`shuyutongbi`     varchar(10),                 // 收入同比%,  
-`shijingniu`      varchar(10),                //  市净率,     
-`meigujingzhi`    varchar(10),                 // 每股净资,   
-`meigugongji`     varchar(10),                 // 每股公积,   
-`meiguweifenpei`  varchar(10),                 // 每股未分配, 
-`meiguxianjinliu` varchar(10),                 // 每股现金流, 
-`maoliniu`        varchar(10),                 //毛利率%,    
-`yinyeilirunniu`  varchar(10),                //  营业利润率%,
-`jinlirunniu`     varchar(10),                 // 净利润率%		
+`siyingniu`       float,                 // 市盈(动,    
+`huoyuedu`        float,                  //活跃度,     
+`lianzhangtiansu` int,                 // 连涨天,     
+`shanrizhangfu`   float,                  //3日涨幅%,   
+`ershirizhangfu`  float,                 // 20日涨幅%,  
+`liushirizhangfu` float,                 // 60日涨幅%,  
+`huanshuoz`       float,               //   换手Z,      
+`liutongsizhiz`   varchar(15),                 // 流通市值Z,  
+`beitaxishuo`     float,                  //贝塔系数,   
+`kaipan`          float,                 // 开盘%,      
+`gudongrenshuo`   int,                 // 股东人数,   
+`renjunchigu`     int,                 // 人均持股,   
+`liruntongbi`     float,                //  利润同比%,  
+`shuyutongbi`     float,                 // 收入同比%,  
+`shijingniu`      float,                //  市净率,     
+`meigujingzhi`    float,                 // 每股净资,   
+`meigugongji`     float,                 // 每股公积,   
+`meiguweifenpei`  float,                 // 每股未分配, 
+`meiguxianjinliu` float,                 // 每股现金流, 
+`maoliniu`        float,                 //毛利率%,    
+`yinyeilirunniu`  float,                //  营业利润率%,
+`jinlirunniu`     float,                 // 净利润率%		
 `date`            DATE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 create index stockcode on stockopendata(code);
-create index stockcode on stockopendata(name);
+create index stockname on stockopendata(name);
 create index stockhanyi on stockopendata(xifenhangye);
 select date,code ,name ,kaipanhuanshuoz,kaipanjine,liangbi,xianliang,liutongsizhi,liutongguyi,xifenhangye from stockopendata where name='中水渔业';
 '''
