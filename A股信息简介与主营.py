62688516e677e8544e893b92f7de7e57cbe4009e
newhackerman <newhackerman@163.com>
2021-03-07 11:50:44 +0800

    usge: A股信息简介与主营.py "stockdesc like '%第一%'"

--- A股信息简介与主营.py
+++ A股信息简介与主营.py
@@ -1,28 +1,34 @@
 # __*__endconding='utf-8'__*__
 #信息采集于同花顺
 import requests as req
-# from lxml import etree
 import  pymysql
-import time,datetime,json
+import time,json,sys
 import tushare as ts
 import pandas as pd
-
+import prettytable as pt
 configfile = 'D:/mysqlconfig.json'
 file = './A股信息简介与主营.csv'
-
+#读取配置文件
 def get_config():
     with open(configfile, encoding="utf-8") as f:
         jsoncontent = json.load(f)
     f.close()
     return jsoncontent
-
+#连接数据库
 def dbconnect():
     # 读取json格式的配置文件
     jsoncontent =get_config()
     conn = pymysql.connect(jsoncontent['host'], jsoncontent['user'], jsoncontent['password'],
                            jsoncontent['database'], charset='utf8')
     return conn
-
+###获取股票代码
+def get_stocklist():
+    tusharecode = get_config()
+    pro = ts.pro_api(tusharecode['tushare'])
+    stocklist=pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
+    stocklist=pd.DataFrame(stocklist)
+    return stocklist
+#获取股票基本信息
 def get_stockInfo(stocklist):
     sn = req.session()
     stockinfo = []  # 存储符合条件的企业
@@ -78,14 +84,24 @@ def get_stockInfo(stocklist):
         stockdesc = jsoncontent['data']['describe']
         if stockdesc is None or stockdesc=='':
             stockdesc=''
+        stockdesc=stockdesc.replace('\n','',-1)
+        if len(stockdesc)>=100:
+            stockdesc=stockdesc[0:100]
         base_business = jsoncontent['data']['base_business']
         if base_business is None or base_business=='':
             base_business = ''
+        base_business=base_business.replace('\n','',-1)
+        if len(base_business)>=300:
+            base_business=base_business[0:300]
         business_scope = jsoncontent['data']['business_scope']
         if business_scope is None or business_scope=='':
             business_scope = ''
+        business_scope=business_scope.replace('\n','',-1)
+        if len(business_scope)>=300:
+            business_scope=business_scope[0:300]
         try:
             str1 = code + '|' + name + '|' + industry + '|' + stockdesc+'|'+base_business+'|'+business_scope
+            print(str1)
         except BaseException as e2:
             print(url1+'取值异常！')
         stockinfo.append(str1)
@@ -96,33 +112,46 @@ def get_stockInfo(stocklist):
         for textvalue in stockinfo:
             fw.write(str(textvalue) + '\n')
     return stockinfo
-###获取股票代码
-def get_stocklist():
-    tusharecode = get_config()
-    pro = ts.pro_api(tusharecode['tushare'])
-    stocklist=pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
-    stocklist=pd.DataFrame(stocklist)
-    return stocklist
+
+def readstockinFile(file):
+    infolists=[]
+    f=open(file,'r',encoding='utf-8')
+    context=f.readlines()
+    for data in context:
+        infolists.append(data)
+    return infolists
 # 将数据入表
 def insertstockinfomation(stockinfos):
+
     if len(stockinfos) == 0:
         return
-    #print(northdataAnalyinfos)
     conn = dbconnect()
     cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
-       # 执行的sql语句
-    sql = '''insert into stockinfomation (CODE,NAME,stockdesc,base_business,business_scope) values (%s,%s,%s,%s,%s)'''
+    sql = '''insert into stockinfomation (CODE,NAME,industry,stockdesc,base_business,business_scope) values (%s,%s,%s,%s,%s,%s)'''
     for datalist in stockinfos:
         row=datalist.split('|')
         CODE=row[0]
         NAME=row[1]
-        industry=row[2]
-        stockdesc=row[3]
-        base_business=row[4]
-        business_scope=row[5]
-        values = (CODE, NAME, industry,stockdesc, base_business, business_scope)
-        # sql = '''select code from stockinfomation where code='''''
+        try:
+            industry=row[2]
+        except BaseException as e1:
+            industry=''
+        try:
+            stockdesc=row[3]
+        except BaseException as e2:
+            stockdesc=''
 
+        try:
+            base_business=row[4]
+        except BaseException as e3:
+            base_business=''
+
+        try:
+            business_scope=row[5]
+        except BaseException as e4:
+            business_scope=''
+
+        values = (CODE, NAME, industry,stockdesc, base_business, business_scope)
         try:
             cursor.execute(sql, values)
                 # print(values,sql)
@@ -132,33 +161,65 @@ def insertstockinfomation(stockinfos):
         conn.commit()
     conn.close()
 
-def Select_stockInfo(self):  # **kwords :表示可以传入多个键值对， *kwords:表示可传入多个参数
-    header = ['日期', '代码', '名称', '行业', '简介', '主营业务', '业务范围']
-    newdate = self.get_page_newdate()
-    outdate = datetime.datetime.strptime(newdate, "%Y-%m-%d")
-    yesterday = self.get_lastDay(outdate)
-    sql = 'select * from stockinfomation where hddate=\'' + newdate + '\'and SHAREHOLDPRICEONE>5 and SHAREHOLDPRICEFIVE>1 and Zdf >-2 and SCode in ( select SCode from northdataAnaly where hddate=\'' + yesterday + '\' and SHAREHOLDPRICEONE<0 )  order by SHAREHOLDPRICEONE desc'
+#######################根据条件查询mysql中的数据#######################
+def select_stockinfos(condiction):
+    tablename='stockinfomation'
+    #dict=indb.file2dict(indb.configfile)  #读取配置信息
+    conn=dbconnect()
+    cursor= conn.cursor(cursor=pymysql.cursors.DictCursor)  #打开游标
+    # condictions= str(condiction).strip('{').strip('}').replace(':','=',1).replace('\'','',2)
+
+    sql = "select CODE, NAME, industry,stockdesc, base_business, business_scope from " + tablename + " where " + condiction + ";"
+
+    #print(sql)
     print(sql)
-    conn = self.dbconnect()
-    cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
-    # print(sql)
-    cursor.execute(sql)
-    resultset = cursor.fetchall()
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+    except BaseException as be:
+        print(be)
+        print("Error: unable to fetch data")
     cursor.close()
-    conn.close()
-    # print(resultset)
-    if resultset:
-        return resultset
+    if results:
+        return results
     else:
         # print('未查询到数据，请更新数据！')
         return None
 
+def formatresults(results):
+    #results   查询到的数据集
+    #header   要输出的表头
+    header=['代码' , '名称', '行业' , '简介' , '主营业务' ]
+    tb = pt.PrettyTable()
+    tb.field_names=header #设置表头
+    tb.align='l'  #对齐方式（c:居中，l居左，r:居右）
+    for row in results:  # 依次获取每一行数据
+        CODE = row['CODE']
+        NAME = row['NAME']
+        industry = row['industry']
+        stockdesc = row['stockdesc']
+        base_business = row['base_business']
+        if len(base_business)>=40:
+            base_business=base_business[0:40]
+        business_scope = row['business_scope']
+
+        # # 打印结果
+        # print('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t' % (
+        # date, code, name, kaipanhuanshuoz, kaipanjine, liangbi, xianliang, liutongsizhi, liutongguyi, xifenhangye))
+        tb.add_row([CODE, NAME, industry,stockdesc, base_business])
+    s=tb.get_html_string()  #获取html格式
+    print(tb)
 
 if __name__ == '__main__':
-    stocklist=get_stocklist()
-    stockInfo=get_stockInfo(stocklist)
-    insertstockinfomation(stockInfo)
-
+    cond=r"stockdesc like '%世界第一%'"
+    var = sys.argv  # 可以接收从外部传入参数
+    # 查个股概念信息，或某概念包含的股票信息
+    try:
+         infolist=select_stockinfos(var[1])
+    except BaseException as e:
+        print('未入查询条件')
+        exit(1)
+    formatresults(infolist)
 
 
 '''CREATE TABLE IF NOT EXISTS `stockinfomation`( 
