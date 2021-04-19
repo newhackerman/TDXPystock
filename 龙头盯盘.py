b65e2292c81ed7695aad07fa188b2cfff18478a8
newhackerman <newhackerman@163.com>
2021-04-19 22:12:38 +0800

    优化：处理容器卡的问题

--- 龙头盯盘.py
+++ 龙头盯盘.py
@@ -3,32 +3,21 @@ import sys
 import requests as req
 import time,re,datetime
 import  prettytable as pt
-# from multiprocessing import Process
+
 from PyQt5.Qt import *
 from PySide2.QtWidgets import *
 
 class RunThread(QThread):
-    # python3,pyqt5与之前的版本有些不一样
-    #  通过类成员对象定义信号对象
-    # _signal = pyqtSignal(str)
     trigger = pyqtSignal()
-
     def __init__(self):
         super().__init__()
         self.stocklist = self.getTopStock()
     def __del__(self):
         self.wait()
-
     def run(self):
-        # 处理你要做的业务逻辑，这里是通过一个回调来处理数据，这里的逻辑处理写自己的方法
-        # wechat.start_auto(self.callback)
-        # self._signal.emit(msg);  可以在这里写信号焕发
-
         self.main()
         time.sleep(20)
         self.trigger.emit()
-
-        # self._signal.emit(msg)
 
     def callback(self, msg):
         # 信号焕发，我是通过我封装类的回调来发起的
@@ -42,8 +31,6 @@ class RunThread(QThread):
         data = {"Params": ["ygts"]}
         try:
             r = req.post(url=url, json=data)
-            # print(r.status_code)
-            # print(r.content)
         except BaseException as b:
             count = 0
             while True:
@@ -69,10 +56,8 @@ class RunThread(QThread):
                 ztbs = str(stock[2]) + '天' + str(stock[3]) + '板'
                 tmpdict = {'code': code, 'ztbs': ztbs}
                 stocklist.append(tmpdict)
-            # print(stocklist)
         return stocklist
 
-        # 获取龙实时行情
 
     def getonlineData_sina(self, stocklist):
         onlinequote = []
@@ -83,18 +68,14 @@ class RunThread(QThread):
             # 取出股票代码
             stocks = []
             for data in stocklist:
-                # print(data)
                 if data['code'][0:2] == '68' or data['code'][0:2] == '60':
                     stock = 'sh' + data['code']
-                    # print(stock)
                 else:
                     stock = 'sz' + data['code']
-                    # print(stock)
                 stocks.append(stock)
 
             url = f'https://hq.sinajs.cn/?list={stocks}'
             url = url.replace('[', '').replace(']', '').replace('\'', '', -1).replace(' ', '', -1)
-            # print(url)
             headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
             }
@@ -103,7 +84,6 @@ class RunThread(QThread):
                 r = req.get(url=url, headers=headers, proxies=proxies)
                 if r'sys_auth="FAILED"' in r.text:
                     print('会话失效！！')
-                # print(r.text)
             except BaseException as B:
                 count = 0
                 while True:
@@ -121,17 +101,13 @@ class RunThread(QThread):
                         continue
             regx = r'var.hq_str_..(\d{6})="(.*?)"'
             rtext = re.findall(regx, r.text, re.M)
-
             for data in rtext:
                 stockcode = data[0]
                 tmpdata = data[1].split(',')
-                '''000001 ['平安银行', '21.460', '21.640', '21.560', '21.730', '21.330', '21.520', '21.560', '38302950', '825170840.990', '42500', '21.520', '4300', '21.510', '45400', '21.500', '10100', '21.490', '142700', '21.480', '6705', '21.560', '43700', '21.570', '147200', '21.580', '48200', '21.590', '107500', '21.600', '2021-04-08', '15:00:03', '00']
-                               '''
                 stockname = tmpdata[0]
                 vol = round(float(tmpdata[8]) / 100, 0)  # 换算成手
                 wanamont = round(float(tmpdata[9]) / 10000, 2)  # 换算成万元
                 price = str(tmpdata[3])  # 当前价格
-                # print(price)
                 zdf = round(((float(price) - float(tmpdata[2])) / float(tmpdata[2])) * 100, 2)
                 if '0.0' in price:
                     zdf = 0
@@ -142,13 +118,9 @@ class RunThread(QThread):
                 onlinequote.append(tmpdict)
         return onlinequote
 
-        # 判断是否为交易日
-
     def isTradeDay(self):
         alldays = self.pro.trade_cal()  # 得到所有日期，到今年年尾
-        # print(alldays)
         tradingdays = alldays[alldays['is_open'] == 1]  # 得到所有交易开盘日
-        # print(tradingdays)
         today = datetime.datetime.now().strftime('%Y%m%d')
         if today in tradingdays['cal_date'].values:
             return True
@@ -182,9 +154,6 @@ class RunThread(QThread):
 
     def main(self):
         Thead = ['代码', '名称', '板数', '涨跌幅', '现价', '成交量', '成交万元', '成交时间']
-        # stocklist = self.getTopStock()
-        # print('返回数据为：')
-        # print(stocklist)
         t1 = '09:20'
         t2 = '15:35'
         now = datetime.datetime.now().strftime("%H:%M")
@@ -197,7 +166,6 @@ class RunThread(QThread):
         now = datetime.datetime.now().strftime("%H:%M")
         # print('----------------------龙头盯盘轮询:' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '----------------------------------------------')
         # now = datetime.datetime.now().strftime("%H:%M")
-
         onlineQuote = self.getonlineData_sina(self.stocklist)
         if onlineQuote is None:
             time.sleep(5)
@@ -214,8 +182,7 @@ class RunThread(QThread):
                 time1 = onlineQuote[i]['成交时间']
                 table.add_row([code, name, ztbs, zdf, price, vol, amt, time1])
             # print(table.get_string())
-            mainw.result=table.get_string()
-
+            mainw.result=table.get_string()  #将值传给主窗口
 
 class MainWindow(QMainWindow):
     result=None
@@ -241,16 +208,12 @@ class MainWindow(QMainWindow):
 
     def Work(self):
         self.thread = RunThread()
-        # print(self.thread)
         self.thread.start()
         self.text1.setText(self.result)
-        # print(self.result)
-        # self.TimeStop()
         self.thread.trigger.connect(self.Work)
 
     def TimeStop(self):
         self.timer1.stop()
-        # print("运行结束用时",self.lcdNumber.value())
         self.t = 0
 
     def TimterupdateTime(self):
@@ -264,12 +227,6 @@ class MainWindow(QMainWindow):
     # 定义槽
     def updateTime(self):
         self.setWindowTitle('龙头盯盘'+'\t\t\t'+time.strftime("%X",time.localtime()))
-    # 定义槽
-    # def onTimerOut(self):
-    #     # self.setWindowTitle('我是小程序')
-    #     # self.setWindowTitle(self.windowTitle()+time.strftime("%X",time.localtime()))
-    #     pass
-
 
 if __name__ == '__main__':
 
